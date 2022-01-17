8d0506f1fd31bfdf45224b47569d699ec8e0821b
aditore <anthonyditore@gmail.com>
2022-01-17 01:17:22 -0800

    completed all tasks, updated javascript for better functionality and added styling, will complete readme next

--- assests/script.js
+++ assests/script.js
@@ -91,7 +91,7 @@ var quizQuestions = [
 /* functions */
 
 //timer function for start button
-startTimer.addEventListener("click",function() {
+function startQuiz() {
   if (timeDone === 0) {
     timeDone = setInterval(function() {
       timeTotal--;
@@ -104,7 +104,9 @@ startTimer.addEventListener("click",function() {
         timer.textContent = "Hope you're happy with your answers!";
         //if timer hits zero go to initials page
         quizComplete();
-        //show message
+        //show message create div within scope
+        var makeDiv = document.createElement("div"); 
+        makeDiv.setAttribute("id", "makeDiv");
         makeDiv.textContent = "You are all done! Please submit your initials into the system above." + "The questions you got correct: " + score + "/" + quizQuestions.length + " :)";
 
         //append
@@ -113,8 +115,7 @@ startTimer.addEventListener("click",function() {
     }, 1000);
   }
   showQuestion(questionIndex);
-})
-
+}
 //function to render all questions to page with a for loop
 function showQuestion(questionIndex) {
   //clear existing data
@@ -146,17 +147,17 @@ function checkAnswer(event) {
 
   //checks if the created li is the correct answer on click
   if (check.matches("li")) {
-    var makeDiv = document.createElement("div");
-    makeDiv.setAttribute("id", "makeDiv");
+    var makeDiv2 = document.createElement("div");
+    makeDiv2.setAttribute("id", "makeDiv");
 
     //adds to score if answer is correct
     if (check.textContent == quizQuestions[questionIndex].correctAnswer) {
       score++;
-      makeDiv.textContent = "Nice job! You are correct!";
+      makeDiv2.textContent = "Nice job! You are correct!";
     } else {
       //user will lose time if the answer is wrong
       timeTotal = timeTotal - timeLoss;
-      makeDiv.textContent = "Oof, that answer is going to hurt you!";
+      makeDiv2.textContent = "Oof, that answer is going to hurt you!";
     }
   }
   //makes sure questions move to the next in order
@@ -165,13 +166,13 @@ function checkAnswer(event) {
   //shows score when quiz is complete in format x/quizQuestions(total length)
   if (questionIndex >= quizQuestions.length) {
     quizComplete();
-    makeDiv.textContent = "You are all done! Please submit your initials into the system above." + "The questions you got correct: " + score + "/" + quizQuestions.length + " :)";
+    makeDiv2.textContent = "You are all done! Please submit your initials into the system above." + "The questions you got correct: " + score + "/" + quizQuestions.length + " :)";
   } else {
     showQuestion(questionIndex);
   }
 
   //appends the div created to show message
-  quiz.appendChild(makeDiv);
+  quiz.appendChild(makeDiv2);
 }
 
 //function to show and append last page when quiz is complete
@@ -195,7 +196,7 @@ function quizComplete() {
     makeP.textContent = "Your time left combined with your score: " + timeScore;
 
     quiz.appendChild(makeP2);
-  }
+  } 
 
   //label
   var makeLabel = document.createElement("label");
@@ -210,9 +211,14 @@ function quizComplete() {
 
   //submit button
   var makeButton = document.createElement("button");
-  makeButton.setAttribute("type", "submit");
+  makeButton.setAttribute("type", "button");
   makeButton.setAttribute("id", "submitButton");
   makeButton.textContent = "SUBMIT";
+
+  //try again
+  var makeButton2 = document.createElement("button");
+  makeButton2.setAttribute("id", "tryAgain");
+  makeButton2.textContent = "TRY AGAIN";
 
   //append
   quiz.appendChild(makeH1);
@@ -220,14 +226,17 @@ function quizComplete() {
   quiz.appendChild(makeLabel);
   quiz.appendChild(makeInput);
   quiz.appendChild(makeButton);
+  quiz.appendChild(makeButton2);
 
   //submit button event listener to store highscores
   makeButton.addEventListener("click", function() {
     var init = makeInput.value;
 
     //prevents a null operation and creates new object that holds the current initials and score
-    if (init === null) {
-      console.log("No initials provided.");
+    //will prompt alert window if no initials provided
+    if (init === '') {
+      window.alert("Please provide initials!");
+      return false;
     } else {
      var totalScore = {
         initials: init,
@@ -252,4 +261,14 @@ function quizComplete() {
     //brings scores to highScores html
     window.location.replace("./highScores.html");
   });
+
+  //try again button starts quiz over
+  makeButton2.addEventListener("click", function() {
+    window.location.replace("./index.html");
+  })
 }
+
+//start quiz!
+startTimer.addEventListener("click", function () {
+  startQuiz();
+})
